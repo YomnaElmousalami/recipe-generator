ff797dfd59d9da905f5906880b04a9aa215636e0
YomnaElmousalami <yomnaelmousalami@gmail.com>
2024-12-15 00:08:24 -0500

    adjusted javascript and html file to make sure it responds when the send button is clicked.

--- static/script.js
+++ static/script.js
@@ -1,3 +1,11 @@
+const input = document.querySelector(".input textarea");
 const send_button = document.querySelector(".input span");
+
+let userMessage;
+
+const handlechat = () => {
+    userMessage = input.value.trim();
+    console.log(userMessage);
+}
 
 send_button.addEventListener("click", handlechat);
