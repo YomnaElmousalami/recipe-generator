3e4b892d2cbde5cf9a8fab1f85e8444e8e3037c3
YomnaElmousalami <yomnaelmousalami@gmail.com>
2024-12-15 14:09:47 -0500

    modified javascript and html so that the messages can be sent back and forth between user and AI bot

--- static/script.js
+++ static/script.js
@@ -7,7 +7,13 @@ let userMessage;
 const inputListener = (message, class_name) =>{
     const chatListener = document.createElement("li");
     chatListener.classList.add(class_name);
-    let content = class_name === "begin_chat" ? `<p>${message}</p>` :  `<p class = "entry_message"></p> <p>${message}</p>`;
+    let content = class_name === "begin_chat" ? `<p>${message}</p>` :  
+    `<span id = "query" class = "logo"> 
+    <p class = "entry_message"> 
+        <img src = "https://attic.sh/iiq017aushlxw6gd3e2mqziazci2" class = "icon_entry" alt = "Logo"> 
+        <span class = "entry_text">${message}</span>
+    </p>
+   </span>`;
     chatListener.innerHTML = content;
     return chatListener;
 }
