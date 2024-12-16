4dee34b12aa11bb6b7fd99b879b478c63f050adb
YomnaElmousalami <yomnaelmousalami@gmail.com>
2024-12-16 13:31:56 -0500

    add POST request

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from text_generation import generate_message
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") #Get request, to display the website
 def index():
     return render_template("index.html")
+
+@app.route("/generateText", methods = ["POST"]) #when user generates method, this will run
+def generate_text():
+    data = request.json
+    text = data.get("text")
+    
+    if not text:
+        return jsonify({"error": "No text provided"}), 400
+    
+    response = generate_message(text)
+    
+    return jsonify({"response": response})
 
 if __name__ == "__main__":
     app.run(debug=True)
