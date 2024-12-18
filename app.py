68afdd69bdc71f25d94b27b61f30d2e389b54a30
YomnaElmousalami <yomnaelmousalami@gmail.com>
2024-12-17 19:18:41 -0500

    moved code to flask app.py

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_file
 from text_generation import generate_message
+
+# Make sure to type: "pip install beautifulsoup4" in the command line
+# Make sure to type "pip install gTTS" in the command line
+from bs4 import BeautifulSoup
+from gtts import gTTS
+import os
 
 app = Flask(__name__)
 
 @app.route("/") #Get request, to display the website
 def index():
     return render_template("index.html")
+
 
 @app.route("/generateText", methods = ["POST"]) #when user generates method, this will run
 def generate_text():
@@ -18,6 +25,27 @@ def generate_text():
     response = generate_message(text)
     
     return jsonify({"response": response})
+
+
+@app.route("/retrieveText", methods = ["POST"]) #when user clicks the convert to audio button
+def retrieve_text():
+    data = request.json
+    html_content = data.get("html", "")
+
+    # Convert HTML to plain text
+    soup = BeautifulSoup(html_content, "html.parser")
+    recipe_text = soup.get_text()
+    language = 'en' #english
+    # slow=False. (the converted audio should 
+    # have a high speed)
+    myobj = gTTS(text=recipe_text, lang=language, slow=False)
+    
+    name = "welcome.mp3"
+    # Saving the converted audio in a mp3 file named
+    # welcome 
+    myobj.save(name)
+    return send_file(name, mimetype="audio/mpeg", as_attachment=False)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
